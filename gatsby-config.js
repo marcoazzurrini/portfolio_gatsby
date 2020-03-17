f9ea4999eba383cb7421e0dc639fffd809322f94
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-03-17 13:09:25 +0100

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: `src/img/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
