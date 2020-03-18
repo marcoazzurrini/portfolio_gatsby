29f06719cdae232928f2d885a6fc9359a081394c
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-03-18 11:31:06 +0100

    implement gatsby image plugin

--- src/Components/work.js
+++ src/Components/work.js
@@ -6,7 +6,8 @@ import { HeadingSecondary, HeadingTertiary } from "../Styles/headers"
 import { SectionHero } from "../Styles/text"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-
+import { css } from "@emotion/core"
+import { theme } from "../Styles/theme"
 const WorkSection = styled.div`
   width: 100%;
   padding-top: ${props => props.theme.pm.pm400};
@@ -23,21 +24,9 @@ const ProjectItem = styled.li`
   align-items: center;
   margin-bottom: ${props => props.theme.pm.pm400};
 
-  img {
-    max-width: 50%;
-    display: inline-block;
-    margin-right: ${props => props.theme.pm.pm300};
-    box-shadow: 3px 6px 8px rgba(0, 0, 0, 0.15);
-    border-radius: 3px;
-  }
-
   &:nth-of-type(even) {
     flex-direction: row-reverse;
     justify-content: flex-end;
-    img {
-      margin-left: ${props => props.theme.pm.pm300};
-      margin-right: 0;
-    }
   }
 
   @media only screen and (max-width: ${props =>
@@ -45,22 +34,34 @@ const ProjectItem = styled.li`
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
-
-    img {
-      max-width: 100%;
-      width: 100%;
-      margin-right: 0;
-      margin-bottom: ${props => props.theme.pm.pm300};
-    }
-
     
   &:nth-of-type(even) {
     flex-direction: column;
     justify-content: flex-start;
-    img {
-      margin-left: 0;
-    }
+    align-items: flex-start;
   }
+`
+const ProjectImage = css`
+  max-width: 100%;
+  display: inline-block;
+  box-shadow: 3px 6px 8px rgba(0, 0, 0, 0.15);
+  margin-right: ${theme.pm.pm300};
+  border-radius: 3px;
+  width: 100%;
+  max-width: 500px;
+  @media only screen and (max-width: ${theme.breakpoints.mobile}) {
+    max-width: 100%;
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: ${theme.pm.pm300};
+    margin-left: 0;
+  }
+`
+
+const ProjectImageReverse = css`
+  ${ProjectImage};
+  margin-right: 0;
+  margin-left: ${theme.pm.pm300};
 `
 
 const ProjectDescription = styled.p`
@@ -110,15 +111,13 @@ const BtnLink = styled.a`
   }
 `
 
-export default function Work() {
+export default function Work(props) {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "img/portfolio.png" }) {
+      imageOne: file(relativePath: { eq: "img/portfolio.png" }) {
         childImageSharp {
-          # Specify a fixed image and fragment.
-          # The default width is 400 pixels
-          fixed {
-            ...GatsbyImageSharpFixed_withWebp
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -135,38 +134,9 @@ export default function Work() {
       <ul>
         <ProjectItem>
           <Img
-            fixed={data.file.childImageSharp.fixed}
+            fluid={data.imageOne.childImageSharp.fluid}
             alt="project screenshot"
-          />
-          <ProjectCol>
-            <HeadingTertiary>
-              Personal <span>Portfolio</span>
-            </HeadingTertiary>
-            <ProjectStack>REACT - EMOTIONJS - GATSBY</ProjectStack>
-            <ProjectDescription>
-              This very website you're now watching. I wanted something fast and
-              with good SEO for my portfolio, so vanilla JS with SCSS seemed
-              like the best fit. The visual effect on the top right of the
-              header is achieved via a light library called animate.js,
-              dynamically modifying a SVG file.
-            </ProjectDescription>
-            <ProjectLinks>
-              <BtnLink
-                href="https://github.com/marcoazzurrini"
-                target="_blank"
-                aria-label="github project"
-                rel="noopener noreferrer"
-                github
-              ></BtnLink>
-              <BtnLink primary></BtnLink>
-            </ProjectLinks>
-          </ProjectCol>
-        </ProjectItem>
-
-        <ProjectItem reverse>
-          <Img
-            fixed={data.file.childImageSharp.fixed}
-            alt="project screenshot"
+            css={ProjectImage}
           />
           <ProjectCol>
             <HeadingTertiary>
@@ -195,8 +165,40 @@ export default function Work() {
 
         <ProjectItem>
           <Img
-            fixed={data.file.childImageSharp.fixed}
+            fluid={data.imageOne.childImageSharp.fluid}
             alt="project screenshot"
+            css={[ProjectImage, ProjectImageReverse]}
+          />
+          <ProjectCol>
+            <HeadingTertiary>
+              Personal <span>Portfolio</span>
+            </HeadingTertiary>
+            <ProjectStack>REACT - EMOTIONJS - GATSBY</ProjectStack>
+            <ProjectDescription>
+              This very website you're now watching. I wanted something fast and
+              with good SEO for my portfolio, so vanilla JS with SCSS seemed
+              like the best fit. The visual effect on the top right of the
+              header is achieved via a light library called animate.js,
+              dynamically modifying a SVG file.
+            </ProjectDescription>
+            <ProjectLinks>
+              <BtnLink
+                href="https://github.com/marcoazzurrini"
+                target="_blank"
+                aria-label="github project"
+                rel="noopener noreferrer"
+                github
+              ></BtnLink>
+              <BtnLink primary></BtnLink>
+            </ProjectLinks>
+          </ProjectCol>
+        </ProjectItem>
+
+        <ProjectItem>
+          <Img
+            fluid={data.imageOne.childImageSharp.fluid}
+            alt="project screenshot"
+            css={ProjectImage}
           />
           <ProjectCol>
             <HeadingTertiary>
